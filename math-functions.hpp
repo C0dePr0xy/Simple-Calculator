589a31d9aa73f38c0f8ffcc5403e9e563372afb3
Saif Darwish <135634179+saifdarwish@users.noreply.github.com>
2024-01-13 21:53:42 +0100

    Update math-functions.hpp

--- math-functions.hpp
+++ math-functions.hpp
@@ -8,7 +8,7 @@
 //Handles addition.
 void adding() {
 	const char* math = "history.mdta";
-    std::ofstream mathdata(math, std::ios::app);
+        std::ofstream mathdata(math, std::ios::app);
 	//Math Operation.
 	std::cout << double1 + double2 << "\n";
 
@@ -24,7 +24,7 @@ void adding() {
 //Handles subtraction.
 void subtracting() {
 	const char* math = "history.mdta";
-    std::ofstream mathdata(math, std::ios::app);
+        std::ofstream mathdata(math, std::ios::app);
 	//Math Operation.
 	std::cout << double1 - double2 << "\n";
 
@@ -46,7 +46,7 @@ void multiplying() {
 	//Generates text file to store last known equation.
 	//mathdata.open("history.mdta");
 	const char* math = "history.mdta";
-    std::ofstream mathdata(math, std::ios::app);
+        std::ofstream mathdata(math, std::ios::app);
 	mathdata << "--------------------------------------------------------\n";
 	mathdata << "User: "<< name<<"\n";
 	mathdata << "[" << double1 << "]" << " * " << "[" << double2 << "]\n";
@@ -63,7 +63,7 @@ void dividing() {
 	//Generates text file to store last known equation.
 	//mathdata.open("history.mdta");
 	const char* math = "history.mdta";
-    std::ofstream mathdata(math, std::ios::app);
+        std::ofstream mathdata(math, std::ios::app);
 	mathdata << "--------------------------------------------------------\n";
 	mathdata << "User: "<< name<<"\n";
 	mathdata << "[" << double1 << "]" << " / " << "[" << double2 << "]\n";
@@ -79,7 +79,7 @@ void modulo() {
 	//Generates text file to store last known equation.
 	//mathdata.open("history.mdta");
 	const char* math = "history.mdta";
-    std::ofstream mathdata(math, std::ios::app);
+        std::ofstream mathdata(math, std::ios::app);
 	mathdata << "--------------------------------------------------------\n";
 	mathdata << "User: "<< name<<"\n";
 	mathdata << "[" << double1 << "]" << " % " << "[" << double2 << "]\n";
@@ -97,30 +97,30 @@ void squareroot_or_powerof() {
 
 	//added this if-statement for operations with 1 number and other math operations (Saif Darwish)
 	if(equationNumber==1){
-		std::cout<<"Your number please: "<<std::endl;
-		double chosenNumber;
-		std::cin>>chosenNumber;
-		std::cout<<"If you want the square root then type \'r\' or if you want to the power then type \'^\'"<<std::endl;
-		char chosenOp;
-		while(std::cin>>chosenOp){
-			if(chosenOp == '^' || chosenOp == 'r'){
-				break;
-			}
-			else{
-				std::cout<<"Please try again. You can only choose \"r\" or \"^\""<<std::endl;
-				std::cin.clear();
-			}
-		}
-		if(chosenOp=='^'){
-			int squareNumber;
-			std::cout<<"Add a power number: "<<std::endl;
-			std::cin>>squareNumber;
-			std::cout<<chosenNumber<<"^"<<squareNumber<<" = "<<pow(chosenNumber,squareNumber)<<std::endl;
+	std::cout<<"Your number please: "<<std::endl;
+	double chosenNumber;
+	std::cin>>chosenNumber;
+	std::cout<<"If you want the square root then type \'r\' or if you want to the power then type \'^\'"<<std::endl;
+	char chosenOp;
+	while(std::cin>>chosenOp){
+	if(chosenOp == '^' || chosenOp == 'r'){
+	break;
+	}
+	else{
+	std::cout<<"Please try again. You can only choose \"r\" or \"^\""<<std::endl;
+	std::cin.clear();
+	}
+	}
+	if(chosenOp=='^'){
+	int squareNumber;
+	std::cout<<"Add a power number: "<<std::endl;
+	std::cin>>squareNumber;
+	std::cout<<chosenNumber<<"^"<<squareNumber<<" = "<<pow(chosenNumber,squareNumber)<<std::endl;
 		
 		
 	//Generates text file to store last known equation.
-    const char* math = "history.mdta";
-    std::ofstream mathdata(math, std::ios::app);
+        const char* math = "history.mdta";
+        std::ofstream mathdata(math, std::ios::app);
 	mathdata.open("history.mdta");
 	mathdata << "--------------------------------------------------------\n";
 	mathdata << "User: "<< name<<"\n";
@@ -129,28 +129,28 @@ void squareroot_or_powerof() {
 	mathdata << "--------------------------------------------------------\n";
 	mathdata.close();
 		    
-		}
-		if(chosenOp=='r'){
-			std::cout<<"The sqaure root of "<<chosenNumber<<" is "<<sqrt(chosenNumber)<<" ."<<std::endl;
+	}
+	if(chosenOp=='r'){
+	std::cout<<"The sqaure root of "<<chosenNumber<<" is "<<sqrt(chosenNumber)<<" ."<<std::endl;
 		
 	//Generates text file to store last known equation.
 	const char* math = "history.mdta";
-    std::ofstream mathdata(math, std::ios::app);
+        std::ofstream mathdata(math, std::ios::app);
 	mathdata.open("history.mdta");
 	mathdata << "--------------------------------------------------------\n";
 	mathdata << "User: "<< name<<"\n";
 	mathdata << "The sqaure root of ["<<chosenNumber<<"] is ["<<sqrt(chosenNumber)<< "]\n";
 	mathdata << "--------------------------------------------------------\n";
 	mathdata.close();
-		}
+	}
 	}
 
 
 }
 
 
-	void clear_history(){
+void clear_history(){
 	const char* math = "history.mdta";
-    std::ofstream mathdata(math, std::ios::trunc);
+        std::ofstream mathdata(math, std::ios::trunc);
 	mathdata.close();
-	}
+}
