6218f7453ebcdf88aed4881873467b7b4045c55f
Eric Guerra <87400651+C0dePr0xy@users.noreply.github.com>
2024-02-01 19:17:16 -0500

    Update math-functions.hpp
    Added proper indentation.

--- math-functions.hpp
+++ math-functions.hpp
@@ -3,141 +3,129 @@
 #include <fstream>
 #include <cmath>
 
-//const char* math = "history.mdta";
-//std::ofstream mathdata(math, std::ios::app);
-//Handles addition.
+void adding();
+void subtracting();
+void multiplying();
+void dividing();
+void modulo();
+void squareroot_or_powerof();
+void clear_history();
+
 void adding() {
-	const char* math = "history.mdta";
-        std::ofstream mathdata(math, std::ios::app);
-	//Math Operation.
-	std::cout << double1 + double2 << "\n";
+    const char* math = "history.mdta";
+    std::ofstream mathdata(math, std::ios::app);
 
-	//Generates text file to store last known equation.
-	mathdata << "--------------------------------------------------------\n";
-	mathdata << "User: "<< name<<"\n";
-	mathdata << "[" << double1 << "]" << " + " << "[" << double2 << "]\n";
-	mathdata << " = " << double1 + double2 << "\n";
-	mathdata << "--------------------------------------------------------\n";
-	mathdata.close();
+    std::cout << double1 + double2 << "\n";
+
+    mathdata << "--------------------------------------------------------\n";
+    mathdata << "User: " << name << "\n";
+    mathdata << "[" << double1 << "]" << " + " << "[" << double2 << "]\n";
+    mathdata << " = " << double1 + double2 << "\n";
+    mathdata << "--------------------------------------------------------\n";
+    mathdata.close();
 }
 
-//Handles subtraction.
 void subtracting() {
-	const char* math = "history.mdta";
-        std::ofstream mathdata(math, std::ios::app);
-	//Math Operation.
-	std::cout << double1 - double2 << "\n";
+    const char* math = "history.mdta";
+    std::ofstream mathdata(math, std::ios::app);
 
-	//Generates text file to store last known equation.
-	//mathdata.open("history.mdta");
-	mathdata << "--------------------------------------------------------\n";
-	mathdata << "User: "<< name<<"\n";
-	mathdata << "[" << double1 << "]" << " - " << "[" << double2 << "]\n";
-	mathdata << " = " << double1 - double2 << "\n";
-	mathdata << "--------------------------------------------------------\n";
-	mathdata.close();
+    std::cout << double1 - double2 << "\n";
+
+    mathdata << "--------------------------------------------------------\n";
+    mathdata << "User: " << name << "\n";
+    mathdata << "[" << double1 << "]" << " - " << "[" << double2 << "]\n";
+    mathdata << " = " << double1 - double2 << "\n";
+    mathdata << "--------------------------------------------------------\n";
+    mathdata.close();
 }
 
-//Handles multiplication.
 void multiplying() {
-	//Math Operation.
-	std::cout << double1 * double2 << "\n";
+    const char* math = "history.mdta";
+    std::ofstream mathdata(math, std::ios::app);
 
-	//Generates text file to store last known equation.
-	//mathdata.open("history.mdta");
-	const char* math = "history.mdta";
-        std::ofstream mathdata(math, std::ios::app);
-	mathdata << "--------------------------------------------------------\n";
-	mathdata << "User: "<< name<<"\n";
-	mathdata << "[" << double1 << "]" << " * " << "[" << double2 << "]\n";
-	mathdata << " = " << double1 * double2 << "\n";
-	mathdata << "--------------------------------------------------------\n";
-	mathdata.close();
+    std::cout << double1 * double2 << "\n";
+
+    mathdata << "--------------------------------------------------------\n";
+    mathdata << "User: " << name << "\n";
+    mathdata << "[" << double1 << "]" << " * " << "[" << double2 << "]\n";
+    mathdata << " = " << double1 * double2 << "\n";
+    mathdata << "--------------------------------------------------------\n";
+    mathdata.close();
 }
 
-//Handles division.
 void dividing() {
-	//Math Operation.
-	std::cout << double1 / double2 << "\n";
+    const char* math = "history.mdta";
+    std::ofstream mathdata(math, std::ios::app);
 
-	//Generates text file to store last known equation.
-	//mathdata.open("history.mdta");
-	const char* math = "history.mdta";
-        std::ofstream mathdata(math, std::ios::app);
-	mathdata << "--------------------------------------------------------\n";
-	mathdata << "User: "<< name<<"\n";
-	mathdata << "[" << double1 << "]" << " / " << "[" << double2 << "]\n";
-	mathdata << " = " << double1 / double2 << "\n";
-	mathdata << "--------------------------------------------------------\n";
-	mathdata.close();
+    std::cout << double1 / double2 << "\n";
+
+    mathdata << "--------------------------------------------------------\n";
+    mathdata << "User: " << name << "\n";
+    mathdata << "[" << double1 << "]" << " / " << "[" << double2 << "]\n";
+    mathdata << " = " << double1 / double2 << "\n";
+    mathdata << "--------------------------------------------------------\n";
+    mathdata.close();
 }
 
 void modulo() {
-	//Modulo operation
-	std::cout<< fmod(double1,double2)<<"\n";
+    const char* math = "history.mdta";
+    std::ofstream mathdata(math, std::ios::app);
 
-	//Generates text file to store last known equation.
-	//mathdata.open("history.mdta");
-	const char* math = "history.mdta";
-        std::ofstream mathdata(math, std::ios::app);
-	mathdata << "--------------------------------------------------------\n";
-	mathdata << "User: "<< name<<"\n";
-	mathdata << "[" << double1 << "]" << " % " << "[" << double2 << "]\n";
-	mathdata << " = " << fmod(double1,double2) << "\n";
-	mathdata << "--------------------------------------------------------\n";
-	mathdata.close();
+    std::cout << fmod(double1, double2) << "\n";
+
+    mathdata << "--------------------------------------------------------\n";
+    mathdata << "User: " << name << "\n";
+    mathdata << "[" << double1 << "]" << " % " << "[" << double2 << "]\n";
+    mathdata << " = " << fmod(double1, double2) << "\n";
+    mathdata << "--------------------------------------------------------\n";
+    mathdata.close();
 }
 
-//Handles square root and power of functionality thanks to Saif Darwish.
 void squareroot_or_powerof() {
-	std::cout<<"If you want the square root then type [r] or if you want to the power then type [^]"<<std::endl;
-	char chosenOp;
-	while(std::cin>>chosenOp){
-	if(chosenOp == '^' || chosenOp == 'r'){
-	break;
-	}
-	else{
-	std::cout<<"Please try again. You can only choose [r] or [^]"<<std::endl;
-	std::cin.clear();
-	}
-	}
-	if(chosenOp=='^'){
-	int squareNumber;
-	std::cout<<"Add a power number: "<<std::endl;
-	std::cin>>squareNumber;
-	std::cout<<double1<<"^"<<squareNumber<<" = "<<pow(double1,squareNumber)<<std::endl;
-		
-		
-	//Generates text file to store last known equation.
+    std::cout << "If you want the square root then type [r] or if you want to the power then type [^]\n";
+
+    char chosenOp;
+    while (std::cin >> chosenOp) {
+        if (chosenOp == '^' || chosenOp == 'r') {
+            break;
+        }
+        else {
+            std::cout << "Please try again. You can only choose [r] or [^]\n";
+            std::cin.clear();
+        }
+    }
+
+    if (chosenOp == '^') {
+        int squareNumber;
+        std::cout << "Add a power number: ";
+        std::cin >> squareNumber;
+        std::cout << double1 << "^" << squareNumber << " = " << pow(double1, squareNumber) << "\n";
+
         const char* math = "history.mdta";
         std::ofstream mathdata(math, std::ios::app);
-	mathdata << "--------------------------------------------------------\n";
-	mathdata << "User: "<< name<<"\n";
-	mathdata << "[" << double1 <<"^"<<squareNumber<< "]\n";
-	mathdata << " = " << pow(double1,squareNumber) << "\n";
-	mathdata << "--------------------------------------------------------\n";
-	mathdata.close();
-		    
-	}
-	if(chosenOp=='r'){
-	std::cout<<"The sqaure root of "<<double1<<" is "<<sqrt(double1)<<" ."<<std::endl;
-		
-	//Generates text file to store last known equation.
-	const char* math = "history.mdta";
+        mathdata << "--------------------------------------------------------\n";
+        mathdata << "User: " << name << "\n";
+        mathdata << "[" << double1 << "^" << squareNumber << "]\n";
+        mathdata << " = " << pow(double1, squareNumber) << "\n";
+        mathdata << "--------------------------------------------------------\n";
+        mathdata.close();
+    }
+
+    if (chosenOp == 'r') {
+        std::cout << "The square root of " << double1 << " is " << sqrt(double1) << " .\n";
+
+        const char* math = "history.mdta";
         std::ofstream mathdata(math, std::ios::app);
-	mathdata << "--------------------------------------------------------\n";
-	mathdata << "User: "<< name<<"\n";
-	mathdata << "The sqaure root of ["<<double1<<"] is ["<<sqrt(double1)<< "]\n";
-	mathdata << "--------------------------------------------------------\n";
-	mathdata.close();
-	}
-
-
+        mathdata << "--------------------------------------------------------\n";
+        mathdata << "User: " << name << "\n";
+        mathdata << "The square root of [" << double1 << "] is [" << sqrt(double1) << "]\n";
+        mathdata << "--------------------------------------------------------\n";
+        mathdata.close();
+    }
 }
 
-
-void clear_history(){
-	const char* math = "history.mdta";
-        std::ofstream mathdata(math, std::ios::trunc);
-	mathdata.close();
+void clear_history() {
+    const char* math = "history.mdta";
+    std::ofstream mathdata(math, std::ios::trunc);
+    mathdata.close();
 }
